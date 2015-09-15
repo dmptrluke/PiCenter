e7e42a0dbc6c00e6926cc11c5edf2281db94301a
Luke <lukeroge@gmail.com>
2015-09-16 02:49:25 +1200

    Add clock and stuff

--- picenter/main.py
+++ picenter/main.py
@@ -1,13 +1,21 @@
+# generic imports
+from time import localtime, strftime
+
 # set config before importing
 from kivy.config import Config
 Config.set('graphics', 'resizable', 0)
 
-# continue importing
+# kivy imports
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+from kivy.uix.tabbedpanel import TabbedPanelItem
+from kivy.uix.floatlayout import FloatLayout
 
-from kivy.properties import ObjectProperty
+
+class MainLayout(FloatLayout):
+    def update(self, *args):
+        self.ids.status_time.text = strftime("%I:%M %p", localtime())
 
 
 class StatusTab(TabbedPanelItem):
@@ -18,18 +26,17 @@ class WeatherTab(TabbedPanelItem):
     pass
 
 
-class MainPanel(TabbedPanel):
-    tab_status = ObjectProperty(None)
-    tab_weather = ObjectProperty(None)
-
-
 class PiCenterApp(App):
     title = 'PiCenter'
 
     def build(self):
+        # set window size
         Window.size = (800, 400)
-        gui = MainPanel()
-        return gui
+
+        # initialise main panel and start updates
+        main_layout = MainLayout()
+        Clock.schedule_interval(main_layout.update, 1)
+        return main_layout
 
 if __name__ == '__main__':
     PiCenterApp().run()
